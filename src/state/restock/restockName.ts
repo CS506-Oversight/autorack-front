3895149d60f21bc74fcf07165c2640a15a585ced
Joziah Mays <jmays2@wisc.edu>
2021-04-06 14:59:49 -0500

    Removed unnecessary action involving sorting

--- src/state/restock/restockName.ts
+++ src/state/restock/restockName.ts
@@ -4,5 +4,4 @@ export enum RestockDispatcherName {
     // TODO: SHOULD BE CONNECTED TO THE ACCOUNT PAGE
     UPDATE_RESTOCK_SETTINGS = 'updateRestockSettings',
     FETCH_RESTOCK_DATA = 'fetchRestockData',
-    SORT_RESTOCK_DATA = 'sortRestockData',
 }
